cd8c43f5144e988abe1b63fcceadedd51ceccb05
anjha1 <achhutanandj@gmail.com>
2024-07-05 02:42:11 +0530

    text modified added m2()

--- python/female_text_to_speech.py
+++ python/female_text_to_speech.py
@@ -5,7 +5,7 @@ voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 engine.setProperty('rate', 130)
 
-text = "hello i am Anjha"
+text = "hello i am Anjha and what is your name"
 
 engine.say(text)
 engine.runAndWait()  
